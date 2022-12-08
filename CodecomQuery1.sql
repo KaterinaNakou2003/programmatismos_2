674122aac4511d127cd9bc66987a42421b4a4b85
lambriniarv <115944916+lambriniarv@users.noreply.github.com>
2022-12-08 18:58:30 +0200

    Add files via upload

--- CodecomQuery1.sql
+++ CodecomQuery1.sql
@@ -3,27 +3,27 @@ create table Users
 			(username varchar(20) not null primary key,
 			password varchar(15) not null);
 create table Messages
-			(message_id int not null primary key,
+			(message_id int not null identity(1,1) primary key,
 			sender varchar(20) not null foreign key references Users(username) on delete cascade,
 			message_body varchar(200) not null);
 create table Replies
-			(reply_id int not null primary key,
+			(reply_id int not null identity(1,1) primary key,
 			message_id int not null foreign key references Messages(message_id) on delete cascade,
 			replied_to varchar(20) not null foreign key references Users(username) on delete no action,
 			replier varchar(20) not null foreign key references Users(username) on delete no action,
 			reply_body varchar(100) not null);
 create table Likes
-			(like_id int not null primary key,
+			(like_id int not null identity(1,1) primary key,
 			message_id int not null foreign key references Messages(message_id) on delete cascade,
 			liked_user varchar(20) not null foreign key references Users(username) on delete no action,
 			liker varchar(20) not null foreign key references Users(username) on delete no action);
 create table Dislikes
-			(dislike_id int not null primary key,
+			(dislike_id int not null identity(1,1) primary key,
 			message_id int not null foreign key references Messages(message_id) on delete cascade,
 			disliked_user varchar(20) not null foreign key references Users(username) on delete no action,
 			disliker varchar(20) not null foreign key references Users(username) on delete no action);
 create table Logout
-			(logout_id int not null primary key,
+			(logout_id int not null identity(1,1) primary key,
 			username varchar(20) not null foreign key references Users(username) on delete cascade,
 			lastmessageseen int not null foreign key references Messages(message_id) on delete no action);
 
