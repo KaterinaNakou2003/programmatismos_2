0881c8a8f5bae722669e8049e90635fe2fced37b
KaterinaNakou2003 <115884979+KaterinaNakou2003@users.noreply.github.com>
2022-12-15 23:34:15 +0200

    Update Message.java

--- Message.java
+++ Message.java
@@ -1,38 +1,119 @@
+package net.codejava.sql;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
 import java.util.Scanner;
 
 public class Message {
 
+	Connection conn = null;
+    Statement st = null;
+    ResultSet rs = null;
+    String SQL_statement = null;
 
-	private Scanner input;
 
-
-	public String setMessage() {
-		input = new Scanner(System.in);
-		return input.next();
-
+	public void getConnectionWithDB() {
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			conn = DriverManager.getConnection(
+				"jdbc:sqlserver://DESKTOP-NVDOEDC:1433;"
+				+ "DatabaseName=Codecom;encrypt=true;trustServerCertificate=true;", "Progr2", "programmatismos2");;
+		} catch (Exception exception) {
+			System.out.println("DB Connection Exception " + exception);
+		}
 	}
-	// stelnei mhnyma
-	public void sendMessage(String username){
+
+	public void sendMessage(String username) {
+		getConnectionWithDB();
 		System.out.println("Type your message");
-		System.out.println(username + ":" +setMessage()); // na fainetai poiow to egrapse
-		
-	}
-	//dexetai mhnyma
-	public void getMessage(String username) {
-		System.out.println(username +"You have x new Messages"); // kapws tha syndeetai me vaseis kai tha dixnei ta posa nea
-		//mhnymata exei
-	}
+		Scanner input = new Scanner(System.in);
+		String answer = input.nextLine();
+		System.out.println(username + ":" + answer);
+		//update db
+		SQL_statement = "INSERT INTO Messages(sender,message_body) VALUES ( '" + username + " ' , '" + answer + "');";
+		try {
+			if (conn != null ) {
+				st = conn.createStatement();
+				st.executeUpdate(SQL_statement);
+				st.close();
+				conn.close();
+			}
+		} catch (SQLException e) {
+			System.out.println("SQL statement exception" + e);
+		}
+
+	}// telos sendMessage
+
+	public void getMessage(String user) {
+		getConnectionWithDB();
+		SQL_statement = "SELECT TOP 1 message_id FROM Messages ORDER BY message_id DESC;";
+		try {
+			if (conn != null ) {
+				st = conn.createStatement();
+				ResultSet lastMessageOfDB = st.executeQuery(SQL_statement);// finds the number of the last message in db
+				lastMessageOfDB.first();
+				int nolmodb =lastMessageOfDB.getInt("message_id");
+				st.close();
+				lastMessageOfDB.close();
+				//find last logout of user
+				SQL_statement = "SELECT MAX(logout_id) FROM Logout WHERE username =" + "'"+ user +"'";
+				try {
+					if (conn != null ) {
+						st = conn.createStatement();
+						ResultSet lastLogout = st.executeQuery(SQL_statement);
+						lastLogout.first();
+						int noll = lastLogout.getInt("logout_id");
+						int result = nolmodb - noll;
+						System.out.println(user +"You have" + result + " new Messages \n You caught up with us!!");
+						st.close();
+						lastLogout.close();
+						conn.close();
+					}
+				} catch (SQLException e) {
+					System.out.println("SQL statement exception" + e);
+				}
+			}
+		} catch (SQLException e) {
+			System.out.println("SQL statement exception" + e);
+
+		}
+	}//telos getMessage
+
 	public void reply(String username, int numberOfMessage) {
+		getConnectionWithDB();
 		System.out.println("Type your message");
-		System.out.println(username + " replied to " + numberOfMessage + " Message :" + setMessage() );
+		Scanner input = new Scanner(System.in);
+		String answer = input.nextLine();
+		//updates Messages table
+		SQL_statement = "INSERT INTO Messages(sender,message_body) VALUES ( '" + username + " ' , '" + answer + "')";
+		try {
+			if (conn != null ) {
+				st = conn.createStatement();
+				st.executeUpdate(SQL_statement);
+				st.close();
+			}
+		} catch (SQLException e) {
+			System.out.println("SQL statement exception" + e);
+		}
+		//prints message to user
+		SQL_statement = "SELECT message_body,sender FROM Messages WHERE message_id = " + numberOfMessage;
+		try {
+			if (conn != null ) {
+				st = conn.createStatement();
+				rs = st.executeQuery(SQL_statement);
+				rs.first();
+				System.out.println(username + " replied to " + rs.getString("message_body") + " : " + answer );
+				st.close();
+				rs.close();
+				conn.close();
+			}
+		} catch (SQLException e) {
+			System.out.println("SQL statement exception" + e);
+		}
 
-	}
-	//tou xrhstou
-	public void likeMessage() {};
-	// addition
-	public void dislikeMessage() {};
+	}//telos reply
 
-	public void problem(int numberOfMessage) {
-		System.err.println(numberOfMessage + "Message not sent");
-	}
-}
+}//telos message
