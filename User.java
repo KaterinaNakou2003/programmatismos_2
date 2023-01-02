85502391593cd3c858b2575a6e67f2eaf8c289b3
KaterinaNakou2003 <115884979+KaterinaNakou2003@users.noreply.github.com>
2023-01-02 23:51:23 +0200

    Update User.java

--- User.java
+++ User.java
@@ -1,5 +1,4 @@
 package net.codejava.sql;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -62,14 +61,14 @@ public class User {
 				int result = account.addAccount(account.getUsername(),account.getPassword());
 
 
-			 	 if (result==1) {
+			 	 if (result == 1) {
 					username = account.getUsername();
 					System.out.println("Welcome " + username);
 					System.out.println("\n\n\n\n\n\n\n\n\n\n");
 					System.out.println("--INSTRUCTIONS for beginners-- ");
 					System.out.println("Change of mind? \n For action 1 to 4 if you want to back out type exit and then press enter in the body text");
 					System.out.println("Please, do not try to find another way to back out so as for CodeCom to	fuction properly... \n Help us Help you!!");
-					System.out.println("\n\n\n\n\n\n\n\n\n\n");
+					System.out.println("\n\n\n\n");
 			  	} else {
 					System.out.println("Person not added:Error");
 				}
@@ -82,21 +81,21 @@ public class User {
 				username=account.getUsername();
 				System.out.println("Sign In Successfull");
 				System.out.println("Hello " + username +". Have fun!" );
-				System.out.println(message.getMessage(username));
+				message.getMessage(username);
 			} else {
 				System.out.println("Account not found");
 			}
 		}
 
-		int action=0, y;
+		int action = 0, y;
 		String answer;
 		System.out.print( "\n What do you want to do: \n 1.Send a message \n 2.Like a message \n 3.Dislike a message \n 4.Reply to message \n 5.Logout \n 6.Instructions \n");
 
 		while (action != 5 || flag != true) {
-			if (action==1) {
+			if (action == 1) {
 				answer = message.printMessage();
 				System.out.println(message.sendMessage(username,answer));
-			} else if (action==2) {
+			} else if (action == 2) {
 				System.out.println("Type the number of the message you want to like: ");
 				y = input.nextInt();
 				SQL_statement = "SELECT TOP 1 message_id FROM Messages ORDER BY message_id DESC;";
@@ -105,7 +104,7 @@ public class User {
 						st = conn.createStatement();
 						rs = st.executeQuery(SQL_statement);// finds the last message in db
 						rs.first();
-						if (rs.getInt("message_id") >= y) {
+						if (rs.getInt("message_id") >= y && y != 0) {
 							//message.likeMessage(username, y);
 						} else {
 							System.out.println("ERROR!! We cannot find the message you want to reply to.");
@@ -117,7 +116,7 @@ public class User {
 					System.out.println("SQL statement exception" + e);
 				}
 				System.out.println();
-			} else if (action==3) {
+			} else if (action == 3) {
 				System.out.println("Type the number of the message you want to dislike: ");
 				y = input.nextInt();
 				SQL_statement = "SELECT TOP 1 message_id FROM Messages ORDER BY message_id DESC;";
@@ -126,7 +125,7 @@ public class User {
 						st = conn.createStatement();
 						rs = st.executeQuery(SQL_statement);// finds the last message in db
 						rs.first();
-						if (rs.getInt("message_id") >= y) {
+						if (rs.getInt("message_id") >= y &&  y != 0) {
 							//message.dislikeMessage(username, y);
 						} else {
 							System.out.println("ERROR!! We cannot find the message you want to reply to.");
@@ -140,14 +139,14 @@ public class User {
 				System.out.println();
 			} else if (action==4) {
 				System.out.print("Type the number of the message you want to reply to: ");
-				 y = input.nextInt();
+				y = input.nextInt();
 				SQL_statement = "SELECT TOP 1 message_id FROM Messages ORDER BY message_id DESC;";
 				try {
 					if (conn != null ) {
 						st = conn.createStatement();
 						rs = st.executeQuery(SQL_statement);// finds the last message in db
 						rs.first();
-						if (rs.getInt("message_id") >= y) {
+						if (rs.getInt("message_id") >= y && y != 0) {
 							answer = message.printMessage();
 							System.out.println(message.reply(username, answer, y));
 						} else {
@@ -191,6 +190,7 @@ public class User {
 				st = conn.createStatement();
 				st.executeUpdate(SQL_statement); // inserts in table logout the number of this last message
 				st.close();
+				conn.close();
 			} catch (SQLException e) {
 				System.out.println("SQL statement exception" + e);
 			}
@@ -199,7 +199,6 @@ public class User {
 			System.out.println("SQL statement exception" + e);
 
 		}
-
-	} // telos main
+	}
 
 } // telos user
