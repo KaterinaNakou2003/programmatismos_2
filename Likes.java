5aaa3fbea120ad09260ff27d83fd9bd3561c1181
krispapa <116659891+krispapa@users.noreply.github.com>
2023-01-04 23:31:37 +0200

    Update Likes.java

--- Likes.java
+++ Likes.java
@@ -1,34 +1,83 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Likes {
+
+	Connection conn = null;
+	Statement st = null;
+	ResultSet rs = null;
+    String SQL_statement = null;
+
 	public String likedUser(String username,int y){
-		connectWithDB();
-		String lkduser = "SELECT sender FROM Messages WHERE message_id ="+ y;
-		SQL_statement = "INSERT INTO Likes(message_id,liked_user,liker) VALUES('"+y+"','"+lkduser+"','"+username+"');";
-		return lkduser;
-		// update db
-		try{
-			st = conn.createStatement();
-			st.executeUpdate(SQL_statement);
-			st.close();
+		connectDB();
+		SQL_statement = "SELECT sender FROM Messages WHERE message_id = y;";
+		try {
+			if (conn != null ) {
+				st = conn.createStatement();
+				rs = st.executeQuery(SQL_statement);
+				String lkduser = rs.getString("sender");
+				st.close();
+				rs.close();
+				conn.close();
+				return lkduser;
+			}
 		}catch (SQLException e){
-			System.out.println("SQL statement exception" + e)
+				System.out.println("SQL statement exception" + e)
 		}
 	}
+
+
+
 	public int likeCounter(int y){
-    	int d = "SELECT COUNT(*) FROM Likes WHERE message_id="+y;
-    	return d;
+		connectDB();
+    	SQL_statement =" SELECT COUNT(*) FROM Likes WHERE message_id=y;";
+    	try {
+			if (conn != null ){
+				st = conn.createStatement();
+				rs = st.executeQuery(SQL_statement);
+				int d = rs.getInt("COUNT");
+				st.close();
+				rs.close();
+				conn.close();
+				return d;
+	        }
+		}catch (SQLException e) {
+			System.out.println("SQL statement exception" + e);
+		}
 	}
-	public void connectWithDB(){
+
+
+
+	public void updateLikes(String username, String likedname int y) {
+		connectDB();
+		SQL_statement = "INSERT INTO Likes(message_id,liked_user,liker) VALUES('"+y+"','"+likedname+"','"+username+"');";
+		try {
+			if (conn != null ) {
+				st = conn.createStatement();
+				st.executeUpdate(SQL_statement);
+				st.close();
+				conn.close();
+			}
+		} catch (SQLException e) {
+			System.out.println("SQL statement exception" + e);
+		}
+	}
+
+
+	public void connectDB(){
 		try {
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 			Connection conn = DriverManager.getConnection(
 					"jdbc:sqlserver://DESKTOP-NVDOEDC:1433;"
 					+ "DatabaseName=Codecom;encrypt=true;trustServerCertificate=true;", "Progr2", "programmatismos2");
-			System.out.println("Yay, we connected it!");
 		} catch (SQLException e) {
 					System.err.println("Connection failed");
-					e.printStackTrace();
-		} catch(ClassNotFoundException cnfe){
-			System.out.println("Error loading driver")
 		}
 	}
+
+
+
 }
