b0ba8dd93ce5fca63c80a3c34a2e4d7b1680f2e0
krispapa <116659891+krispapa@users.noreply.github.com>
2023-01-06 02:26:54 +0200

    Update Dislikes.java

--- Dislikes.java
+++ Dislikes.java
@@ -1,40 +1,82 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Dislikes {
-
-	public String dislikedUser(String username,int y){
-
-			connectWithDB();
-			String dislkduser = "SELECT sender FROM Messages WHERE message_id = y;";
-			SQL_statement = "INSERT INTO Dislikes(message_id,disliked_user,disliker) VALUES('"+y+"','"+dislkduser+"','"+username+"');";
-			return dislkduser;
-			// update db
-			try{
-				st = conn.createStatement();
-				st.executeUpdate(SQL_statement);
-				st.close();
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+	    String SQL_statement = null;
+	
+		public String dislikedUser(int y){
+			connectDB();
+			SQL_statement = "SELECT sender FROM Messages WHERE message_id = "+y+";";
+			try {
+				if (conn != null ) {
+					st = conn.createStatement();
+					rs = st.executeQuery(SQL_statement);
+					String dislkduser = rs.getString("sender");
+					st.close();
+					rs.close();
+					conn.close();
+					return dislkduser;
+				}
 			}catch (SQLException e){
+					System.out.println("SQL statement exception" + e);
+			}
+		}
+	
+	
+	
+		public int dislikeCounter(int y){
+			connectDB();
+	    	SQL_statement =" SELECT COUNT(*) FROM Dislikes WHERE message_id="+y+";";
+	    	try {
+				if (conn != null ){
+					st = conn.createStatement();
+					rs = st.executeQuery(SQL_statement);
+					int d = rs.getInt("COUNT");
+					st.close();
+					rs.close();
+					conn.close();
+					return d;
+		        }
+			}catch (SQLException e) {
 				System.out.println("SQL statement exception" + e);
 			}
-
 		}
-
-		public int dislikeCounter(int y){
-	    	int d = "SELECT COUNT(*) FROM Dislikes WHERE message_id=y;";
-	    	return d;
-
+	
+	
+	
+		public void updateDislikes(String username, String dislikedname, int y) {
+			connectDB();
+			SQL_statement = "INSERT INTO Dislikes(message_id,disliked_user,disliker) VALUES('"+y+"','"+dislikedname+"','"+username+"');";
+			try {
+				if (conn != null ) {
+					st = conn.createStatement();
+					st.executeUpdate(SQL_statement);
+					st.close();
+					conn.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("SQL statement exception" + e);
+			}
 		}
-
-
-		public void connectWithDB(){
+	
+	
+		public void connectDB(){
 			try {
 				Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 				Connection conn = DriverManager.getConnection(
 						"jdbc:sqlserver://DESKTOP-NVDOEDC:1433;"
 						+ "DatabaseName=Codecom;encrypt=true;trustServerCertificate=true;", "Progr2", "programmatismos2");
-				System.out.println("Yay, we connected it!");
 			} catch (SQLException e) {
 						System.err.println("Connection failed");
-						e.printStackTrace();
 			}
 		}
+	
+	
+	
 }
-
