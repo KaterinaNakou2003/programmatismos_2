47ffcca4362ada193eb5cfeed1f58a84b264d4ca
krispapa <116659891+krispapa@users.noreply.github.com>
2023-01-09 21:58:50 +0200

    Update Likestest.java

--- Likestest.java
+++ Likestest.java
@@ -1,46 +1,41 @@
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
-public class LikesTest {
+public class TestLikes {
 
-	@Test
-	public void testLikedUser() {
-		Likes likes = new Likes();
-		String expected = "John Doe";
-		String actual = likes.likedUser(1);
-		assertEquals(expected, actual);
+    @Test
+    public void testLikedUser() {
+        Likes like = new Likes();
+        String expected = "John";
+        String actual = like.likedUser(1);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testMessageBody() {
+        Likes like = new Likes();
+        String expected = "Hello World!";
+        String actual = like.messageBody(1);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testLikeCounter() {
+        Likes like = new Likes();
+        int expected = 3;
+        int actual = like.likeCounter(1);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+		public void testUpdateLikes() {
+			Likes like = new Likes();
+		    String username = "Maria";
+			String likedname = "John";
+			int y = 1;
+			int expected = 4;
+			like.updateLikes(username, likedname, y);
+			int actual = like.likeCounter(1);
+			assertEquals(expected, actual);
 	}
-
-	@Test
-	public void testMessageBody() {
-		Likes likes = new Likes();
-		String expected = "This is a test message";
-		String actual = likes.messageBody(1);
-		assertEquals(expected, actual);
-	}
-
-	@Test
-	public void testLikeCounter() {
-		Likes likes = new Likes();
-		int expected = 10;
-		int actual = likes.likeCounter(1);
-		assertEquals(expected, actual);
-	}
-
-	@Test
-	public void testUpdateLikes() {
-		Likes likes = new Likes();
-
-	        String username = "John Doe";
-		String likedname = "Jane Doe";
-		int y = 1;
-		int expected = 11;
-		likes.updateLikes(username, likedname, y);
-		int actual = likes.likeCounter(1);
-		assertEquals(expected, actual);
-	}
-
-
 }
-
