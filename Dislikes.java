c4327e99d6e1e5fc3c3552a92c16088155dd2f44
KaterinaNakou2003 <115884979+KaterinaNakou2003@users.noreply.github.com>
2023-01-09 19:20:57 +0200

    Update Dislikes.java

--- Dislikes.java
+++ Dislikes.java
@@ -9,82 +9,81 @@ public class Dislikes {
 		Statement st = null;
 		ResultSet rs = null;
 	    String SQL_statement = null;
+		Message message;
 
-		public String dislikedUser(int y){
+		public String dislikedUser(int y) {
 			connectDB();
-			SQL_statement = "SELECT sender, FROM Messages WHERE message_id = "+y+";";
+			String dislkduser = null;
+			y =+ 1002;
+			SQL_statement = "SELECT sender, FROM Messages WHERE message_id = " + y +";";
 			try {
 				if (conn != null ) {
 					st = conn.createStatement();
 					rs = st.executeQuery(SQL_statement);
-					String dislkduser = rs.getString("sender");
+					dislkduser = rs.getString("sender");
 					st.close();
 					rs.close();
-					conn.close();
-					return dislkduser;
+
 				}
 			}catch (SQLException e){
 					System.out.println("SQL statement exception" + e);
-					return "null";
 			}
-			return "null";
+			return dislkduser;
 		}
 
 
-		public String messageBody(int y){
+		public String messageBody(int y) {
 			connectDB();
-			SQL_statement = "SELECT message_body, FROM Messages WHERE message_id = "+y+";";
+			String msgbody = null;
+			y =+ 1002;
+			SQL_statement = "SELECT message_body, FROM Messages WHERE message_id = " + y + ";";
 			try {
 				if (conn != null ) {
 					st = conn.createStatement();
 					rs = st.executeQuery(SQL_statement);
-					String msgbody = rs.getString("message_body");
+					msgbody = rs.getString("message_body");
 					st.close();
 					rs.close();
-					conn.close();
-					return msgbody;
 				}
-			}catch (SQLException e){
-					System.out.println("SQL statement exception" + e);
-					return "null";
-			}
-			return "null";
-		}
-
-
-
-
-		public int dislikeCounter(int y){
-			connectDB();
-	    	SQL_statement =" SELECT COUNT(*) FROM Dislikes WHERE message_id="+y+";";
-	    	try {
-				if (conn != null ){
-					st = conn.createStatement();
-					rs = st.executeQuery(SQL_statement);
-					int d = rs.getInt("COUNT");
-					st.close();
-					rs.close();
-					conn.close();
-					return d;
-		        }
 			}catch (SQLException e) {
-				System.out.println("SQL statement exception" + e);
-				return -1;
+					System.out.println("SQL statement exception" + e);
 			}
-			return -1;
+			return msgbody;
 		}
 
 
 
-		public void updateDislikes(String username, String dislikedname, int y) {
+
+		public int dislikeCounter(int y) {
 			connectDB();
-			SQL_statement = "INSERT INTO Dislikes(message_id,disliked_user,disliker) VALUES('"+y+"','"+dislikedname+"','"+username+"');";
+			int d = -1;
+	    	SQL_statement =" SELECT COUNT(*) FROM Dislikes WHERE message_id=" + y + ";";
+	    	try {
+				if (conn != null ) {
+					st = conn.createStatement();
+					rs = st.executeQuery(SQL_statement);
+					d = rs.getInt("COUNT");
+					st.close();
+					rs.close();
+		        }
+			} catch (SQLException e) {
+				System.out.println("SQL statement exception" + e);
+			}
+			return d;
+		}
+
+
+
+		public void updateDislikes(String username, String dislikedName, int y) {
+			connectDB();
+			SQL_statement = "INSERT INTO Dislikes(message_id,disliked_user,disliker) VALUES('" + y + "','" + dislikedName + "','" + username + "');";
 			try {
 				if (conn != null ) {
 					st = conn.createStatement();
 					st.executeUpdate(SQL_statement);
+					String answer = messageBody(y);
+					message.updateMessages(username,answer,-2);
 					st.close();
-					conn.close();
 				}
 			} catch (SQLException e) {
 				System.out.println("SQL statement exception" + e);
@@ -92,13 +91,13 @@ public class Dislikes {
 		}
 
 
-		public void connectDB(){
+		public void connectDB() {
 			try {
 				Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 				Connection conn = DriverManager.getConnection(
 						"jdbc:sqlserver://DESKTOP-NVDOEDC:1433;"
 						+ "DatabaseName=Codecom;encrypt=true;trustServerCertificate=true;", "Progr2", "programmatismos2");
-			} catch (SQLException e) {
+			} catch (Exception e) {
 						System.err.println("Connection failed");
 			}
 		}
