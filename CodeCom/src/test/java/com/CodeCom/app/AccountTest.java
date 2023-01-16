316f658f73029ee61d5f58e3e811f236765affa3
FanisBak <116720806+FanisBak@users.noreply.github.com>
2023-01-16 23:58:31 +0200

    Add files via upload

---
+++ CodeCom/src/test/java/com/CodeCom/app/AccountTest.java
@@ -0,0 +1,18 @@
+package com.CodeCom.app;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class AccountTest {
+    @Test
+    public void testAddAccount() {
+        // setup
+        Account account = new Account();
+
+        // exercise
+        int result = account.addAccount("testuser", "testpass");
+
+        // verify
+        assertEquals(1, result);
+    }
+}
