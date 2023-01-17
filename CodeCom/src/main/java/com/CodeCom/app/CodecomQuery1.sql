0a5d3907569f628d9f14a87f28dfa631660cda31
lambriniarv <115944916+lambriniarv@users.noreply.github.com>
2023-01-17 21:31:19 +0200

    Update CodecomQuery1.sql

--- CodeCom/src/main/java/com/CodeCom/app/CodecomQuery1.sql
+++ CodeCom/src/main/java/com/CodeCom/app/CodecomQuery1.sql
@@ -3,17 +3,17 @@ create table Users
 			(username varchar(20) not null primary key,
 			password varchar(15) not null);
 create table Messages
-			(message_id int not null identity(1,1) primary key,
+			(message_id int not null identity(1,1) primary key, /* balte identity(1003,1) */
 			sender varchar(20) not null foreign key references Users(username) on delete cascade,
 			message_body varchar(200) not null,
 			typeofmessage int not null);
 create table Likes
-			(like_id int not null identity(1,1) primary key,
+			(like_id int not null identity(1,1) primary key, /* balte identity(17,1) */
 			message_id int not null foreign key references Messages(message_id) on delete cascade,
 			liked_user varchar(20) not null foreign key references Users(username) on delete no action,
 			liker varchar(20) not null foreign key references Users(username) on delete no action);
 create table Logout
-			(logout_id int not null identity(1,1) primary key,
+			(logout_id int not null identity(1,1) primary key, /* balte identity(2008,1) */
 			username varchar(20) not null foreign key references Users(username) on delete cascade,
 			lastmessageseen int not null foreign key references Messages(message_id) on delete no action);
 
